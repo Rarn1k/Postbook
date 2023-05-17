8eaf21e5067d21360f5f40c1c0cd94442bd287ec
Rarn1k <alyosha.plotnikov2001@mail.ru>
2023-05-17 11:38:13 +0300

    finished tests

---
+++ core/user/tests/test_viewsets.py
@@ -0,0 +1,44 @@
+import pytest
+from rest_framework import status
+
+from core.fixtures.user import user
+from core.fixtures.post import post
+
+
+class TestUserViewSet:
+    endpoint = '/api/user/'
+
+    def test_list(self, client, user):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1
+
+    def test_retrieve(self, client, user):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint + str(user.public_id) + "/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["id"] == user.public_id.hex
+        assert response.data["username"] == user.username
+        assert response.data["email"] == user.email
+
+    def test_create(self, client, user):
+        client.force_authenticate(user=user)
+        data = {}
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+    def test_update(self, client, user):
+        client.force_authenticate(user=user)
+        data = {
+            "username": "test_username_updated",
+            "first_name": "test_first",
+            "last_name": "test_second",
+            "email": "test@mail.ru",
+        }
+        response = client.patch(self.endpoint + str(user.public_id) + "/", data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["username"] == data["username"]
+        assert response.data["first_name"] == data["first_name"]
+        assert response.data["last_name"] == data["last_name"]
+        assert response.data["email"] == data["email"]
